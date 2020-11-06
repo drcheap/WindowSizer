af52eb287aaf13a779c7be9db640c1675f85f1bb
drcheap <drcheap@users.noreply.github.com>
2020-11-06 01:33:32 -0600

    Improve init
    Reworked the initialization code to be a bit more robust.
    
    Also properly initializes the advanced settings, fixing #15 .

--- background.js
+++ background.js
@@ -1,11 +1,42 @@
-const STORAGE_VERSION = 2;
+const CURRENT_STORAGE_VERSION = 2;
 
 async function initialize(details)
 {
    console.log("(init) Initializing...");
 
    // Preset initialization, if needed
-   let storage = await browser.storage.local.get(["presets","version"]);
+   let storage = await browser.storage.local.get(["version","presets","options","advanced"]);
+   if(storage.version === undefined)
+   {
+      // Had non-versioned preset storage, so treat it as version 1
+      storage.version = 1;
+   }
+
+   if(Number.isInteger(storage.version))
+   {
+      if(storage.version < CURRENT_STORAGE_VERSION)
+      {
+         // Storage is old, do any necessary upgrades to remain compatible
+
+         if(storage.version == 1)
+         {
+            console.log("(update) Migrating storage from version 1 to 2...");
+            storage.version = 2;
+         }
+
+         if(storage.version == 2)
+         {
+            // (future storage updates here)
+         }
+
+         await browser.storage.local.set({"version": storage.version});
+      }
+   }
+   else
+   {
+      console.log("(init) No storage version tag found, initializing...");
+      await browser.storage.local.set({"version": CURRENT_STORAGE_VERSION});
+   }
 
    if(!Array.isArray(storage.presets))
    {
@@ -13,38 +44,26 @@ async function initialize(details)
       storage.presets = [];
       await browser.storage.local.set({"presets": storage.presets});
    }
-   else
+
+   if(storage.options === undefined)
    {
-      if(storage.version === undefined)
-      {
-         // Had non-versioned preset storage, so treat it as version 1
-         storage.version = 1;
-      }
+      console.log("(init) No options found, initializing...");
+      storage.options = {};
+      await browser.storage.local.set({"options": storage.options});
    }
 
-   if(Number.isInteger(storage.version))
+   if(storage.advanced === undefined)
    {
-      if(storage.version < STORAGE_VERSION)
-      {
-         // Storage is old, do any necessary upgrades to remain compatible
-
-         if(storage.version == 1)
-         {
-            console.log("(update) Migrating storage from version 1 to 2...");
-            await browser.storage.local.set({"version": 2});
-            storage.version = 2;
-         }
-
-         // (future storage updates here)
-      }
-   }
-   else
-   {
-      console.log("(init) No storage version tag found, initializing...");
-      await browser.storage.local.set({"version": STORAGE_VERSION});
+      console.log("(init) No advanced settings found, initializing...");
+      storage.advanced = {};
+      await browser.storage.local.set({"advanced": storage.advanced});
    }
 
-   console.log("(init) Initialization complete, " + storage.presets.length + " presets found");
+   console.log("(init) Initialization complete...");
+   console.log("(init)    Storage version: " + storage.version);
+   console.log("(init)    Preset count: " + storage.presets.length);
+   console.log("(init)    Option count: " + Object.keys(storage.options).length);
+   console.log("(init)    Advanced count: " + Object.keys(storage.advanced).length);
 }
 
 async function handleCommand(command)
